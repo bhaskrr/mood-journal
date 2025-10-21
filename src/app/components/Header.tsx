908f105e573a2c7b35469a949fe24e512dd7b528
bhaskrr <74664799+bhaskrr@users.noreply.github.com>
2025-10-21 13:26:06 +0530

    added header component

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function Header(props: {
+  h1: string;
+  p: string;
+  linkRoute: string;
+  linkName: string;
+}) {
+  return (
+    <header className="flex justify-between items-center">
+      <div>
+        <h1>{props.h1}</h1>
+        <p>{props.p}</p>
+      </div>
+      <nav className="flex gap-4">
+        <Link href={props.linkRoute}>
+          <Button>{props.linkName}</Button>
+        </Link>
+        <Link href="/new">
+          <Button>New Entry</Button>
+        </Link>
+      </nav>
+    </header>
+  );
+}
