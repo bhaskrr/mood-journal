78906ce17d1e125123866929cf4f3df3d53db0f4
bhaskrr <74664799+bhaskrr@users.noreply.github.com>
2025-10-25 23:47:21 +0530

    improved journalcard component styling

--- src/app/components/JournalCard.tsx
+++ src/app/components/JournalCard.tsx
@@ -1,4 +1,4 @@
-import { cn } from "@/lib/utils";
+import { Calendar } from "lucide-react";
 
 interface JournalCardProps {
   dayOfWeek: string;
@@ -14,14 +14,14 @@ export default function JournalCard({
   text,
 }: JournalCardProps) {
   return (
-    <div className="group relative flex flex-col my-6 bg-white dark:bg-slate-900 border border-slate-200 dark:border-slate-800 rounded-xl w-96 transition-all duration-300 hover:shadow-lg">
-      <div className="p-6 space-y-4">
+    <div className="md:w-160 lg:w-200 xl:w-300 flex flex-col my-6 bg-white dark:bg-slate-900 border border-slate-200 dark:border-slate-800 rounded-xl w-96 transition-all duration-300 hover:shadow-lg">
+      <div className="p-6 space-y-4 max-w-full">
         {/* Header */}
-        <div className="border-b border-slate-200 dark:border-slate-800 pb-4">
-          <h5 className="text-2xl font-semibold text-slate-800 dark:text-slate-100">
-            {dayOfWeek}
+        <div className="flex items-center gap-3 border-b border-slate-200 dark:border-slate-800 pb-4">
+          <span><Calendar  /></span>
+          <h5 className=" font-semibold text-slate-700 dark:text-slate-100">
+            {dayOfWeek}, {date}
           </h5>
-          <p className="text-sm text-slate-500 dark:text-slate-400">{date}</p>
         </div>
 
         {/* Content */}
