cbb3c140927d88d99413bfa46f9f98bdbac4e6f6
bhaskrr <74664799+bhaskrr@users.noreply.github.com>
2025-11-01 23:10:54 +0530

    Added form component

---
+++ src/app/components/Form.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+const formSchema = z.object({
+  text: z.string().min(5, { error: "Text should be at least 5 characters." }),
+});
+
+export default function InputForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      text: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-xl">
+        <FormField
+          control={form.control}
+          name="text"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-2xl">Describe Your Day</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Today I felt... because..."
+                  {...field}
+                  rows={7}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
